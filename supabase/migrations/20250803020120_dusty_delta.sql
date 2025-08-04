5e6b3eb8da9e3646a58109495c5f7f7249f5b60d
TsuBr1988 <gustavo.suzigan.leis@gmail.com>
2025-08-04 11:31:56 -0300

    Updated 20250803020120_dusty_delta.sql

--- supabase/migrations/20250803020120_dusty_delta.sql
+++ supabase/migrations/20250803020120_dusty_delta.sql
@@ -42,26 +42,16 @@ ON CONFLICT DO NOTHING;
 
 -- Inserir cidades com ISS (baseado na tabela padrão brasileira)
 INSERT INTO budget_cities (city_name, iss_percent) VALUES 
-('São Paulo - SP', 5.00),
-('Rio de Janeiro - RJ', 5.00),
-('Belo Horizonte - MG', 5.00),
-('Salvador - BA', 5.00),
-('Brasília - DF', 5.00),
-('Fortaleza - CE', 5.00),
-('Manaus - AM', 5.00),
-('Curitiba - PR', 5.00),
-('Recife - PE', 5.00),
-('Goiânia - GO', 5.00),
-('Belém - PA', 5.00),
+('São Paulo - SP', 2.00),
 ('Guarulhos - SP', 5.00),
-('Campinas - SP', 2.00),
-('Nova Iguaçu - RJ', 3.00),
-('Maceió - AL', 5.00),
-('São Luís - MA', 5.00),
-('Duque de Caxias - RJ', 3.00),
-('Natal - RN', 5.00),
-('Teresina - PI', 5.00),
-('Campo Grande - MS', 5.00)
+('Campinas - SP', 5.00),
+('Piracicaba - SP', 5.00)  
+('Americana - SP', 3.00)  
+('Santa Bárbara DOeste - SP', 2.00)
+('Sumaré - SP', 5.00)
+('Hortolândia - SP', 2.00)
+('Paulínia - SP', 5.00)
+('São Carlos - SP', 2.00)
 ON CONFLICT DO NOTHING;
 
 -- Habilitar RLS
